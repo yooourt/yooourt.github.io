020278576cf74046038076c9b028b45261f6f43b
tao_home <tao@doocom.cn>
2021-02-23 21:19:31 +0800

    edit main.user.js

--- addons/main.user.js
+++
@@ -1,9 +0,0 @@
-// ==UserScript==
-// @name         test_user_script
-// @namespace    test_user_script
-// @version      0.1
-// @author       twt
-// @match        *://*/*
-// ==/UserScript==
-
-console.log('@@ hello')
